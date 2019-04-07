f5dd84d9ab0a7840774e0fa0bf09231997c345b4
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-07 03:03:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   # your code here
   my_new_array = []
   array.each do |number|
-    value = number **2
+    value = number**2
+    my_new_array << value
   end
   my_new_array
 end
